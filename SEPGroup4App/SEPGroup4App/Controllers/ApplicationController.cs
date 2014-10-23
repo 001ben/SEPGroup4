49c094ff4f01ff29e16335f2e55b5b99f8effffd
Ben Brown <bufflanky@gmail.com>
2014-10-23 17:18:48 +1100

    Created an initial section and some folders

---
+++ SEPGroup4App/SEPGroup4App/Controllers/ApplicationController.cs
@@ -0,0 +1,32 @@
+﻿using SEPGroup4App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEPGroup4App.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ApplicantDetails()
+        {
+            // Get data from database
+
+            ApplicantDetailsViewModel model = new ApplicantDetailsViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Section1(ApplicantDetailsViewModel model)
+        {
+            return RedirectToAction("Section2");
+        }
+    }
+}
---
+++ SEPGroup4App/SEPGroup4App/ViewModels/ApplicationSection1ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SEPGroup4App.ViewModels
+{
+    public enum ApplicantType {
+        ResearchStudent = 0, Staff = 1
+    }
+
+    public class ApplicantDetailsViewModel
+    {
+        //[Display(Name="Call this matt")]
+        [Required]
+        public int? ApplicationId { get; set; }
+
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "School/Unit")]
+        public string SchoolUnit { get; set; }
+        public string Supervisor { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Application Type")]
+        public ApplicantType? ApplicantType { get; set; }
+        [Display(Name = "Is this your first application this year?")]
+        public bool? FirstApplicationThisYear { get; set; }
+    }
+}
