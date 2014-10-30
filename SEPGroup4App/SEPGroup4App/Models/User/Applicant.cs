113382dc807dca314e3e3fd9c48c0e1319f4dd94
Thomas-Pelchen <asynchronousnightmare@gmail.com>
2014-10-30 17:32:45 +1100

    Added User based model classes.

---
+++ SEPGroup4App/SEPGroup4App/Models/User/Applicant.cs
@@ -0,0 +1,19 @@
+﻿using SEPGroup4App.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEPGroup4App.Models.User
+{
+    public class Applicant : User
+    {
+        public string Surname { get; set; }
+        public string FirstName { get; set; }
+        public string SchoolUnit { get; set; }
+        public string Supervisor { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
