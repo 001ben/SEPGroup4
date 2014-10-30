3d1180b0cc939d279f50d1eabc79fd082652e87d
Ben Brown <bufflanky@gmail.com>
2014-10-30 16:50:14 +1100

    Started making the back-end database

---
+++ SEPGroup4App/SEPGroup4App/Models/Application/ApplicantDetails.cs
@@ -0,0 +1,28 @@
+﻿using SEPGroup4App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SEPGroup4App.Models
+{
+    public class ApplicantDetails
+    {
+        [Key, ForeignKey("Application"), DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ApplicationId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SchoolUnit { get; set; }
+        public string Supervisor { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public ApplicantType? ApplicantType { get; set; }
+        public bool? FirstApplicationThisYear { get; set; }
+
+        [Required]
+        public virtual Application Application { get; set; }
+    }
+}
---
+++ SEPGroup4App/SEPGroup4App/Models/Application/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SEPGroup4App.Models
+{
+    public class Application
+    {
+        [Key]
+        public int ApplicationId { get; set; }
+        public bool Submitted { get; set; }
+
+        public DateTime? SubmittedOn { get; set; }
+        public string StaffStudentId { get; set; }
+
+        public virtual ApplicantDetails ApplicantDetails { get; set; }
+    }
+}
