db516572b2ea1e2d2d7927b6bb454512b647c7ae
Peggy <peggy@peggy>
2014-11-05 15:27:13 +1100

    user create view

--- SEPGroup4App/SEPGroup4App/ViewModels/UserCreateViewModel.cs
+++ SEPGroup4App/SEPGroup4App/ViewModels/UserCreateViewModel.cs
@@ -15,6 +15,13 @@ namespace SEPGroup4App.ViewModels
         public string UserName { get; set; }
         [Display(Name = "Email")]
         public string Email { get; set; }
-        
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string Surname { get; set; }
+        [Display(Name = "School Unit")]
+        public string SchoolUnit { get; set; }
+        public string Supervisor { get; set; }
+        public string Phone { get; set; }
     }
 }
