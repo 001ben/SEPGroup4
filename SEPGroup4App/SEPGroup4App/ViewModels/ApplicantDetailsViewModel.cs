6079063b54aa99d7994ca2d7bc722710291c9cb1
thomas-pelchen <asynchronousnightmare@gmail.com>
2014-11-05 20:45:49 +1100

    Smexy number update.

--- SEPGroup4App/SEPGroup4App/ViewModels/ApplicantDetailsViewModel.cs
+++ SEPGroup4App/SEPGroup4App/ViewModels/ApplicantDetailsViewModel.cs
@@ -13,9 +13,11 @@ namespace SEPGroup4App.ViewModels
         public int? ApplicationId { get; set; }
 
         [Display(Name="First Name")]
+        [RegularExpression(@"[A-Za-z]+", ErrorMessage="Names must only contain letters.")]
         [Required]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
+        [RegularExpression(@"[A-Za-z]+", ErrorMessage="Names must only contain letters.")]
         [Required]
         public string LastName { get; set; }
         [Display(Name = "School/Unit")]
@@ -27,10 +29,12 @@ namespace SEPGroup4App.ViewModels
         [Required]
         public string Email { get; set; }
         [Display(Name = "Phone Number")]
+        [RegularExpression(@"^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$", ErrorMessage="Please enter a valid Australian phone number.")]
         [Phone]
         [Required]
         public string PhoneNumber { get; set; }
         [Display(Name = "Applicant Type")]
+        
         [Required]
         public ApplicantType? ApplicantType { get; set; }
         [Display(Name = "Is this your first application this year?")]
--- SEPGroup4App/SEPGroup4App/ViewModels/UserCreateViewModel.cs
+++ SEPGroup4App/SEPGroup4App/ViewModels/UserCreateViewModel.cs
@@ -8,21 +8,29 @@ namespace SEPGroup4App.ViewModels
 {
     public class UserCreateViewModel
     {
-
+        [RegularExpression(@"\d{1,10}", ErrorMessage="ID has to only contain numbers and be less than 10 characters.")]
         [Display(Name = "UTS ID")]
         [Required]
         public string StaffStudentID { get; set; }
         [Display(Name = "User Name")]
+        [Required]
         public string UserName { get; set; }
+        [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [RegularExpression(@"[A-Za-z]+", ErrorMessage = "Names must only contain letters.")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
+        [RegularExpression(@"[A-Za-z]+", ErrorMessage = "Names must only contain letters.")]
         public string Surname { get; set; }
         [Display(Name = "School Unit")]
         public string SchoolUnit { get; set; }
         public string Supervisor { get; set; }
+
+        [Phone]
+        [RegularExpression(@"^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$", ErrorMessage = "Please enter a valid Australian phone number.")]
         public string Phone { get; set; }
     }
 }
